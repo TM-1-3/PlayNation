d62b37c97eaba5a6eb7f1ec907028e28358d585f
gab1demattos <gab1demattos@yahoo.com>
2025-11-26 02:24:53 +0000

    implementing user search

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -5,15 +5,17 @@
 @section('content')
 
 <div id="search-user">
-    <form id="search-user" action="{{ route('search.users') }}" method="GET">
+    <form id="search-user-input" action="{{ route('search.users') }}" method="GET">
         <input type="text" id="search-input-user" name="search" placeholder="Search by Name, Username or Email...">
     </form>
-    @foreach($users as $user)
-    <div class="user-search">
-        <a href="{{ route('profile.show',$user->id_user) }}" class="search-name">{{ $user->name }}</a>
-        <a href="{{ route('profile.show',$user->id_user) }}" class="search-username">{{ $user->username }}</a>
-    </div>
-    @endforeach
+    <div id="users-list">
+        @foreach($users as $user)
+        <div class="user-search">
+            <a href="{{ route('profile.show',$user->id_user) }}" class="search-name">{{ $user->name }}</a>
+            <a href="{{ route('profile.show',$user->id_user) }}" class="search-username">{{ $user->username }}</a>
+        </div>
+        @endforeach
+    <div>
 </div>
 
 @endsection
