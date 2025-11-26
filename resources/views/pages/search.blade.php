a5366aa3ce5eb14d1cc2982b683ca088391335ff
gab1demattos <gab1demattos@yahoo.com>
2025-11-26 02:12:08 +0000

    added search bar

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -5,6 +5,10 @@
 @section('content')
 
 <div id="search-user">
+    <form id="search-user" action="{{ route('search.users') }}" method="GET">
+        <input type="text" id="search-input-user" name="search" placeholder="Search by Name, Username or Email...">
+        <button class="btn-primary" type="submit" style="background-color: #3498db;">Search</button>
+    </form>
     @foreach($users as $user)
     <div class="user-search">
         <a href="{{ route('profile.show',$user->id_user) }}" class="search-name">{{ $user->name }}</a>
