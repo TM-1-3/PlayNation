62c48d09d3196fcd9585aafd526776d9985cba48
gab1demattos <gab1demattos@yahoo.com>
2025-12-12 00:08:15 +0000

    group search type

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -296,59 +296,68 @@ class GroupController extends Controller
         } else {
             $groups = Group::all();
         }
-
-        if ($request->ajax()) {
-            $myGroups = collect();
-            $otherGroups = collect();
-
-            if ($user) {
-                // user's group IDs (member or owner)
-                $memberGroupIds = DB::table('group_membership')
-                    ->where('id_member', $user->id_user)
-                    ->pluck('id_group')
-                    ->toArray();
-                
-                $ownerGroupIds = DB::table('groups')
-                    ->where('id_owner', $user->id_user)
-                    ->pluck('id_group')
-                    ->toArray();
-
-                $myGroupIds = array_unique(array_merge($memberGroupIds, $ownerGroupIds));
-
-                // myGroups and otherGroups
-                $myGroups = $groups->whereIn('id_group', $myGroupIds)->values();
-                $otherGroups = $groups->whereNotIn('id_group', $myGroupIds)
-                    ->sortByDesc('is_public')
-                    ->values();
-            } else {
-                // public groups for visitors
-                $otherGroups = $groups->where('is_public', true)->values();
-            }
-
-            // pictures
-            $myGroups = $myGroups->map(function($group) {
-                $groupArray = $group->toArray();
-                $groupArray['picture'] = $group->getGroupPicture();
-                return $groupArray;
-            });
-
-            $otherGroups = $otherGroups->map(function($group) {
-                $groupArray = $group->toArray();
-                $groupArray['picture'] = $group->getGroupPicture();
-                return $groupArray;
-            });
-
-            return response()->json([
-                'myGroups' => $myGroups,
-                'otherGroups' => $otherGroups
-            ]);
-        }
         
         if ($type == 'group-page') {
-            return view('pages.admin', ['groups' => $groups, 'type' => 'group']);
+            if ($request->ajax()) {
+                $myGroups = collect();
+                $otherGroups = collect();
+
+                if ($user) {
+                    // user's group IDs (member or owner)
+                    $memberGroupIds = DB::table('group_membership')
+                        ->where('id_member', $user->id_user)
+                        ->pluck('id_group')
+                        ->toArray();
+                    
+                    $ownerGroupIds = DB::table('groups')
+                        ->where('id_owner', $user->id_user)
+                        ->pluck('id_group')
+                        ->toArray();
+
+                    $myGroupIds = array_unique(array_merge($memberGroupIds, $ownerGroupIds));
+
+                    // myGroups and otherGroups
+                    $myGroups = $groups->whereIn('id_group', $myGroupIds)->values();
+                    $otherGroups = $groups->whereNotIn('id_group', $myGroupIds)
+                        ->sortByDesc('is_public')
+                        ->values();
+                } else {
+                    // public groups for visitors
+                    $otherGroups = $groups->where('is_public', true)->values();
+                }
+
+                // pictures
+                $myGroups = $myGroups->map(function($group) {
+                    $groupArray = $group->toArray();
+                    $groupArray['picture'] = $group->getGroupPicture();
+                    return $groupArray;
+                });
+
+                $otherGroups = $otherGroups->map(function($group) {
+                    $groupArray = $group->toArray();
+                    $groupArray['picture'] = $group->getGroupPicture();
+                    return $groupArray;
+                });
+
+                return response()->json([
+                    'myGroups' => $myGroups,
+                    'otherGroups' => $otherGroups
+                ]);
+            }
         } else if ($type == 'group-admin') {
-            return view('pages.admin', ['groups' => $groups, 'type' => 'group']);
-    }
+            if ($request->ajax()) {
+                // pictures
+                $groups = $groups->map(function($group) {
+                    $groupArray = $group->toArray();
+                    $groupArray['picture'] = $group->getGroupPicture();
+                    return $groupArray;
+                });
+
+                return response()->json([
+                    'groups' => $groups,
+                ]);
+            }
         }
+    }
         
 }
