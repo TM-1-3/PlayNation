5cbd6b785f585b867c72732af3d7065b13389a7c
gab1demattos <gab1demattos@yahoo.com>
2025-12-16 20:22:45 +0000

    modifyed home top

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,32 +5,32 @@
 @section('content')
 
 <div id="main-container" class="max-w-3xl mx-auto transition-transform duration-300 ease-in-out px-5">
-    <div class="fixed bg-gray-50 w-[54vw] pt-5">
-    @auth
-    <div>
-        <div class="flex gap-4 justify-around items-center mb-5">
-            <a href="{{ route('home', ['timeline' => 'public']) }}" class="flex-1 text-center py-3 no-underline text-gray-500 font-semibold text-sm border-b-2 max-w-[200px] transition-colors {{ (isset($activeTimeline) && $activeTimeline === 'public') ? 'text-gray-800 border-gray-800' : 'border-transparent' }}"> Recent</a>
-            <a href="{{ route('home', ['timeline' => 'personalized']) }}" class="flex-1 text-center py-3 no-underline text-gray-500 font-semibold text-sm border-b-2 max-w-[200px] transition-colors {{ (isset($activeTimeline) && $activeTimeline === 'personalized') ? 'text-gray-800 border-gray-800' : 'border-transparent' }}">For You</a>
-            <a href="{{ route('home', ['timeline' => 'following']) }}" class="flex-1 text-center py-3 no-underline text-gray-500 font-semibold text-sm max-w-[200px] transition-colors">Friends</a>
-        </div>
-    </div>
-    @endauth
-    <div class="mb-2 mx-auto flex justify-between">
-        <form  id="search-home" action="{{ route('search.posts') }}" method="GET" class="relative">
-                            
-            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <i class="fa-solid fa-magnifying-glass text-gray-400"></i>
-            </div>
-                                
-            <input id="search-input-home" type="text" name="search" placeholder="Search for posts by username, tags or description.." 
-                    class="h-[2em] w-[50vw] block pl-10 pr-24 py-3 border-none rounded-lg shadow-md text-gray-900  bg-white outline-none">        
-        </form>
+    <div class="fixed w-auto pt-5">
+        @auth
         <div>
-            <button id="filter-toggle" class="flex items-center" aria-expanded="false" aria-controls="filter-panel">
-                <i class="fa-solid fa-sliders text-gray-500 mr-2 cursor-pointer mt-2"></i>
-            </button>
+            <div class="flex gap-4 justify-around items-center mb-5">
+                <a href="{{ route('home', ['timeline' => 'public']) }}" class="flex-1 text-center py-3 no-underline text-gray-500 font-semibold text-sm border-b-2 max-w-[200px] transition-colors {{ (isset($activeTimeline) && $activeTimeline === 'public') ? 'text-gray-800 border-gray-800' : 'border-transparent' }}"> Recent</a>
+                <a href="{{ route('home', ['timeline' => 'personalized']) }}" class="flex-1 text-center py-3 no-underline text-gray-500 font-semibold text-sm border-b-2 max-w-[200px] transition-colors {{ (isset($activeTimeline) && $activeTimeline === 'personalized') ? 'text-gray-800 border-gray-800' : 'border-transparent' }}">For You</a>
+                <a href="{{ route('home', ['timeline' => 'following']) }}" class="flex-1 text-center py-3 no-underline text-gray-500 font-semibold text-sm max-w-[200px] transition-colors">Friends</a>
+            </div>
         </div>
-    </div>
+        @endauth
+        <div class="mb-2 mx-auto flex gap-5 justify-between">
+            <form  id="search-home" action="{{ route('search.posts') }}" method="GET" class="relative">
+                                
+                <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <i class="fa-solid fa-magnifying-glass text-gray-400"></i>
+                </div>
+                                    
+                <input id="search-input-home" type="text" name="search" placeholder="Search for posts by username, tags or description.." 
+                        class="h-[2em] w-[50vw] block pl-10 pr-24 py-3 border-none rounded-lg shadow-md text-gray-900  bg-white outline-none">        
+            </form>
+            <div>
+                <button id="filter-toggle" class="flex items-center" aria-expanded="false" aria-controls="filter-panel">
+                    <i class="fa-solid fa-sliders text-gray-500 mr-2 cursor-pointer mt-2"></i>
+                </button>
+            </div>
+        </div>
     </div>
     <div id="timeline" class="w-full pb-12 pt-35">
             @if(isset($posts) && $posts->isEmpty())
